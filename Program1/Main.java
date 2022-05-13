077f4040b60d1996cc8e5424d9c9489628dc3787
Deepak M S <souparnikad40@gmail.com>
2022-05-13 22:26:12 +0530

    problem1 finished

--- Program1/Main.java
+++ Program1/Main.java
@@ -7,10 +7,25 @@ public class Main{
        //create object for scanner class
        Scanner scan = new Scanner(System.in);
        //read the string input from the user
-       
+       System.out.print("Enter the String : ");
+       String inpuString = scan.nextLine();
        //push each character to the stack
+       for(int i=0;i<inpuString.length();i++){
+           skObj.push(inpuString.charAt(i));
+       }
        //make the new string(reversed) by the stack
+       char[] revSArray = new char[inpuString.length()];
+       for(int i=0;i<inpuString.length();i++){
+           revSArray[i]=skObj.pop();
+       }
+       String newString= String.valueOf(revSArray);
        //compare the two string
+       if(inpuString.compareTo(newString)==0){
+           System.out.println("Entered String is Palindrome !");
+
+       }else{
+           System.out.println("Entered String is Not Palindrome!");
+       }
 
        scan.close();
     }
